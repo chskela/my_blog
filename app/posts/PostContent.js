88f2b7f7fb8ba4e2b8b85d71f6f2404179f97a8d
Aleksey Chistyakov <chskela@gmail.com>
2021-07-23 15:21:11 +0300

    update posts text, posts-utils, PostContent

--- app/posts/PostContent.js
+++ app/posts/PostContent.js
@@ -1,30 +1,32 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
-// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
-import html from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import materialOceanic from "react-syntax-highlighter/dist/cjs/styles/prism/material-oceanic";
 
 import classes from "./class.module.css";
 
 SyntaxHighlighter.registerLanguage("bash", bash);
-SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("js", js);
 
 export default function PostContent({ post }) {
-  const { title, content, slug } = post;
+  const { content, slug } = post;
 
   const customRenderers = {
     p: ({ node, ...props }) => {
       if (node.children[0].tagName === "img") {
         const image = node.children[0];
-
+        const src = `/assets/blog/${slug}/${image.properties.src}`;
+        console.log(node);
         return (
           <div className={classes.image}>
             <Image
-              src={image.properties.src}
+              src={src}
               alt={image.properties.alt}
+              // sizes="50vw"
               width={680}
+              layout="responsive"
               height={440}
             />
           </div>
@@ -32,12 +34,26 @@ export default function PostContent({ post }) {
       }
       return <p {...props} />;
     },
-    code: ({ node, ...props }) =>
-      props?.className === "language-bash" ? (
-        <SyntaxHighlighter language="bash" {...props} style={materialOceanic} />
-      ) : (
-        <p className={classes.inline} {...props} />
-      ),
+    code: ({ node, ...props }) => {
+      if (props?.className === "language-bash") {
+        return (
+          <SyntaxHighlighter
+            {...props}
+            language="bash"
+            style={materialOceanic}
+          />
+        );
+      }
+
+      if (props?.className === "language-js") {
+        return (
+          <SyntaxHighlighter {...props} language="js" style={materialOceanic} />
+        );
+      }
+
+      const { inline, ...restProps } = props;
+      return <code className={classes.inline_code} {...restProps} />;
+    },
     a: ({ node, ...props }) => <a className={classes.link} {...props} />,
   };
 
