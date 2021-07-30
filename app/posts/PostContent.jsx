1169f1643494a477aad27d84c8a1041ef5d20b5c
Aleksey Chistyakov <chskela@gmail.com>
2021-07-30 20:51:52 +0300

    add search posts in Tags page

--- app/posts/PostContent.jsx
+++ app/posts/PostContent.jsx
@@ -7,8 +7,8 @@ import materialOceanic from "react-syntax-highlighter/dist/cjs/styles/prism/mate
 
 import classes from "./class.module.css";
 
-SyntaxHighlighter.registerLanguage("bash", bash);
 SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("bash", bash);
 
 export default function PostContent({ post }) {
   const { content, slug } = post;
@@ -18,13 +18,12 @@ export default function PostContent({ post }) {
       if (node.children[0].tagName === "img") {
         const image = node.children[0];
         const src = `/assets/blog/${slug}/${image.properties.src}`;
-        console.log(node);
+
         return (
           <div className={classes.image}>
             <Image
               src={src}
               alt={image.properties.alt}
-              // sizes="50vw"
               width={680}
               layout="responsive"
               height={440}
@@ -34,6 +33,7 @@ export default function PostContent({ post }) {
       }
       return <p {...props} />;
     },
+
     code: ({ node, ...props }) => {
       if (props?.className === "language-bash") {
         return (
@@ -54,6 +54,7 @@ export default function PostContent({ post }) {
       const { inline, ...restProps } = props;
       return <code className={classes.inline_code} {...restProps} />;
     },
+
     a: ({ node, ...props }) => <a className={classes.link} {...props} />,
   };
 
--- app/tags/Tags.jsx
+++ app/tags/Tags.jsx
@@ -1,28 +1,70 @@
-import { useRouter } from "next/router";
-
 import classes from "./class.module.css";
 
+import { useState, useEffect } from "react";
+
 import { MuiSearch } from "@components/icons";
+import MuiCard from "@components/MuiCard/MuiCard";
 import MuiButton from "@components/MuiButton/MuiButton";
 import MuiTextField from "@components/MuiTextField/MuiTextField";
 
-export default function Tags({ tags }) {
-  const router = useRouter();
+export default function Tags({ tags, posts }) {
+  const [searchValue, setSearchValue] = useState("");
+
+  const tagsLowerCase = tags.map((tag) => tag.toLowerCase());
+
+  const [filteredTags, setFilteredTags] = useState(tagsLowerCase);
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  useEffect(() => {
+    setFilteredTags(tagsLowerCase.filter((tag) => tag.includes(searchValue)));
+    setFilteredPosts(
+      posts.filter(
+        (post) =>
+          post.title.toLowerCase().includes(searchValue) ||
+          post.tags.some((tag) => tag.toLowerCase().includes(searchValue))
+      )
+    );
+  }, [searchValue]);
+
+  function searcHandler(e) {
+    setSearchValue(e.target.value);
+  }
+
+  function clickHandler(e) {
+    e.preventDefault();
+    const value = e.target.innerText.replace(/\#/, "");
+    setSearchValue(value);
+  }
 
   return (
     <div className={classes.content}>
-      <div>
-        <MuiTextField
-          icon={<MuiSearch size={30} />}
-          className={classes.search}
-        />
-      </div>
+      <MuiTextField
+        type="text"
+        value={searchValue}
+        onChange={searcHandler}
+        className={classes.search}
+        icon={<MuiSearch size={30} />}
+        placeholder="Найдите темы, которые вам интересны ..."
+      />
+
       <div className={classes.tags_list}>
-        {tags.map((tag) => (
+        {filteredTags.map((tag) => (
           <MuiButton
-            label={`#${tag.toLowerCase()}`}
             key={tag}
+            onClick={clickHandler}
             className={classes.tag}
+            label={`#${tag}`}
+          />
+        ))}
+      </div>
+
+      <div className={classes.articles}>
+        {filteredPosts.map((post, index) => (
+          <MuiCard
+            article={post}
+            href="/"
+            className={classes.small}
+            key={index}
           />
         ))}
       </div>
